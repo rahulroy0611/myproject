b8e5d02abc596d4fa6c4660b4aa07f3f52ac5bea
rahulroy0611 <rahulroy0611@gmail.com>
2025-06-05 19:12:16 +0000

    fix

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import './App.css';
 import * as React from 'react';
 import Header from './includes/Header.js';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
 import Dashboard from './Dashboard.js';
 
 export default function App () {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Dashboard />} />
+        <Route index path="/" element={<Layout />}>
         </Route>
       </Routes>
     </BrowserRouter>
