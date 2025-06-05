b3bc1605fe311c0e25a8eacbb74c8f46b92bf09c
rahulroy0611 <rahulroy0611@gmail.com>
2025-06-05 19:10:54 +0000

    n

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import * as React from 'react';
 import Header from './includes/Header.js';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Dashboard from './Dashboard.js';
 
-function App() {
+export default function App () {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ src/Dashboard.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Header from './includes/Header.js';
+
+function Dashboard() {
+  return (
+    <div className="Dashboard">
+      <Header />
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
+
+export default Dashboard;
